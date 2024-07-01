065852f77be0f09ac4d6e0f0ea78fe07c17caed1
dmitrii <ppddnk@gmail.com>
2024-07-01 13:42:01 +0300

    еще юнит тесты

--- src/test/java/com/raiffeisen/banking/controller/ApplicationControllerUnitTest.java
+++ src/test/java/com/raiffeisen/banking/controller/ApplicationControllerUnitTest.java
@@ -3,8 +3,9 @@ package com.raiffeisen.banking.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.raiffeisen.banking.entity.Account;
 import com.raiffeisen.banking.entity.AccountStatus;
-import com.raiffeisen.banking.exception.UserNotFoundException;
+import com.raiffeisen.banking.exception.*;
 import com.raiffeisen.banking.model.AccountDTO;
+import com.raiffeisen.banking.model.ChangeBalanceDTO;
 import com.raiffeisen.banking.model.NewAccountDTO;
 import com.raiffeisen.banking.service.AccountService;
 import com.raiffeisen.banking.service.UserService;
@@ -19,9 +20,10 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.math.BigDecimal;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 
 @ExtendWith(MockitoExtension.class)
 @WebMvcTest(controllers = ApplicationController.class)
@@ -42,11 +44,21 @@ class ApplicationControllerUnitTest {
     Integer existingUserId = 1;
     Integer nonExistingUserId = 999;
 
-    Integer existingAccountId = 1;
+    Integer existingOpenAccountId = 1;
+    Integer existingClosedAccountId = 2;
+    Integer existingNegativeMoneyAmountAccountId = 3;
     Integer nonExistingAccountId = 999;
 
     BigDecimal positiveMoneyAmount = new BigDecimal(100.00);
     BigDecimal negativeMoneyAmount = new BigDecimal(-100.00);
+
+    BigDecimal positiveMoneyDelta = new BigDecimal(10.00);
+    BigDecimal negativeMoneyDelta = new BigDecimal(-10.00);
+
+    ChangeBalanceDTO positiveExistingAccountChangeBalanceDTO = new ChangeBalanceDTO(existingOpenAccountId, positiveMoneyDelta);
+    ChangeBalanceDTO positiveClosedAccountChangeBalanceDTO = new ChangeBalanceDTO(existingClosedAccountId, positiveMoneyDelta);
+    ChangeBalanceDTO negativeExistingAccountChangeBalanceDTO = new ChangeBalanceDTO(existingOpenAccountId, negativeMoneyDelta);
+    ChangeBalanceDTO positiveNonExistingAccountChangeBalanceDTO = new ChangeBalanceDTO(nonExistingAccountId, positiveMoneyDelta);
 
     Integer accountStatusOpenId = 1;
     Integer accountStatusClosedId = 1;
@@ -94,20 +106,87 @@ class ApplicationControllerUnitTest {
 
     class CloseAccountTests {
         @Test
-        @Order(1)
         void closeAccount_Success() {
             Account accountToClose = new Account();
-            accountToClose.setId(existingAccountId);
+            accountToClose.setId(existingOpenAccountId);
             accountToClose.setMoneyAmount(positiveMoneyAmount);
             accountToClose.setUserId(existingUserId);
             accountToClose.setStatus(openStatus);
 
-            AccountDTO expectedClosedAccountDTO = new AccountDTO(existingAccountId, positiveMoneyAmount, existingUserId, accountStatusClosedId);
+            AccountDTO expectedClosedAccountDTO = new AccountDTO(existingOpenAccountId, positiveMoneyAmount, existingUserId, accountStatusClosedId);
 
-            when(mockAccountService.closeAccount(existingAccountId)).thenReturn(expectedClosedAccountDTO);
+            when(mockAccountService.closeAccount(existingOpenAccountId)).thenReturn(expectedClosedAccountDTO);
 
-            assertEquals(expectedClosedAccountDTO, applicationController.closeAccount(existingUserId, existingAccountId));
-            verify(mockAccountService, times(1)).closeAccount(existingAccountId);
+            assertEquals(expectedClosedAccountDTO, applicationController.closeAccount(existingUserId, existingOpenAccountId));
+            verify(mockAccountService, times(1)).closeAccount(existingOpenAccountId);
+        }
+
+        @Test
+        void closeAccount_Fail_AccountNotFoundException() {
+
+            when(mockAccountService.closeAccount(nonExistingAccountId)).thenThrow(new AccountNotFoundException(nonExistingAccountId));
+
+            assertThrows(AccountNotFoundException.class, () -> applicationController.closeAccount(existingUserId, nonExistingAccountId));
+            verify(mockAccountService, times(1)).closeAccount(nonExistingAccountId);
+        }
+
+        @Test
+        void closeAccount_Fail_AccountAlreadyClosedException() {
+
+            when(mockAccountService.closeAccount(existingClosedAccountId)).thenThrow(new AccountAlreadyClosedException(existingClosedAccountId));
+
+            assertThrows(AccountAlreadyClosedException.class, () -> applicationController.closeAccount(existingUserId, existingClosedAccountId));
+            verify(mockAccountService, times(1)).closeAccount(existingClosedAccountId);
+
+        }
+
+        @Test
+        void closeAccount_Fail_AccountCanNotBeClosed() {
+
+            when(mockAccountService.closeAccount(existingNegativeMoneyAmountAccountId)).thenThrow(new AccountCanNotBeClosedException(existingNegativeMoneyAmountAccountId));
+
+            assertThrows(AccountCanNotBeClosedException.class, () -> applicationController.closeAccount(existingUserId, existingNegativeMoneyAmountAccountId));
+            verify(mockAccountService, times(1)).closeAccount(existingNegativeMoneyAmountAccountId);
+        }
+    }
+
+    @Nested
+    class DepositAccountTests {
+        @Test
+        void depositAccount_Success() {
+            AccountDTO expectedAccountDTOAfterDeposit = new AccountDTO(existingOpenAccountId, positiveMoneyAmount.add(positiveMoneyDelta), existingUserId, accountStatusOpenId);
+
+            when(mockAccountService.depositAccount(positiveExistingAccountChangeBalanceDTO)).thenReturn(expectedAccountDTOAfterDeposit);
+
+            assertEquals(expectedAccountDTOAfterDeposit, applicationController.depositAccount(positiveExistingAccountChangeBalanceDTO));
+            verify(mockAccountService, times(1)).depositAccount(positiveExistingAccountChangeBalanceDTO);
+        }
+
+        @Test
+        void depositAccount_Fail_DepositOrWithdrawalNotPositiveValueException() {
+
+            when(mockAccountService.depositAccount(negativeExistingAccountChangeBalanceDTO)).thenThrow(new DepositOrWithdrawalNotPositiveValueException(negativeMoneyDelta));
+
+            assertThrows(DepositOrWithdrawalNotPositiveValueException.class, () -> applicationController.depositAccount(negativeExistingAccountChangeBalanceDTO));
+            verify(mockAccountService, times(1)).depositAccount(negativeExistingAccountChangeBalanceDTO);
+        }
+
+        @Test
+        void depositAccount_Fail_AccountNotFoundException() {
+
+            when(mockAccountService.depositAccount(positiveNonExistingAccountChangeBalanceDTO)).thenThrow(new AccountNotFoundException(nonExistingAccountId));
+
+            assertThrows(AccountNotFoundException.class, () -> applicationController.depositAccount(positiveNonExistingAccountChangeBalanceDTO));
+            verify(mockAccountService, times(1)).depositAccount(positiveNonExistingAccountChangeBalanceDTO);
+        }
+
+        @Test
+        void depositAccount_Fail_AccountAlreadyClosedException() {
+
+            when(mockAccountService.depositAccount(positiveClosedAccountChangeBalanceDTO)).thenThrow(new AccountAlreadyClosedException(existingClosedAccountId));
+
+            assertThrows(AccountAlreadyClosedException.class, () -> applicationController.depositAccount(positiveClosedAccountChangeBalanceDTO));
+            verify(mockAccountService, times(1)).depositAccount(positiveClosedAccountChangeBalanceDTO);
         }
     }
 }
