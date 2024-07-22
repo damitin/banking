18c364f0c4e7a3089ecfea74d3dee706ccbf576b
dmitrii <ppddnk@gmail.com>
2024-07-22 14:34:06 +0300

    Заинжектил конфигурацию Кафки через код. Сделал свой бин kafkaTemplate.

--- src/main/java/com/bankname/banking/kafka/config/KafkaConfig.java
+++ src/main/java/com/bankname/banking/kafka/config/KafkaConfig.java
@@ -1,14 +1,58 @@
 package com.bankname.banking.kafka.config;
 
+import com.bankname.banking.model.AccountDTO;
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
 public class KafkaConfig {
+
+    @Value("${spring.kafka.producer.bootstrap-servers}")
+    private String bootstrapServers;
+
+    @Value("${spring.kafka.producer.key-serializer}")
+    private String keySerializer;
+
+    @Value("${spring.kafka.producer.value-serializer}")
+    private String valueSerializer;
+
+    @Value("${spring.kafka.producer.retries}")
+    private String retries;
+
+    @Value("${spring.kafka.producer.properties.retry.backoff.ms}")
+    private String retryBackoffMs;
+
+    Map<String, Object> producerConfigs() {
+        Map<String, Object> config = new HashMap<>();
+
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, keySerializer);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, valueSerializer);
+        config.put(ProducerConfig.RETRIES_CONFIG, retries);
+        config.put(ProducerConfig.RETRY_BACKOFF_MS_CONFIG, retryBackoffMs);
+
+        return config;
+    }
+
+    @Bean
+    ProducerFactory<String, AccountDTO> producerFactory() {
+        return new DefaultKafkaProducerFactory<>(producerConfigs());
+    }
+
+    @Bean
+    KafkaTemplate<String, AccountDTO> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
 
     @Bean
     NewTopic openAccountTopic() {
